f097342fc2963a36e3955ef171c68a85f98cf2a7
Solution7777 <soumen.gstsolution@gmail.com>
2024-10-22 19:42:22 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Card from "@/components/card";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="p-8 flex gap-4 ">
+    <Card cardHeading={"Income tax e-filing"} cardDetails={"Upload your cocuments and get one click solution for income tax filing 2024"} />
+    <Card cardHeading={"GST e-filing"} cardDetails={"Upload your invoice and get filed pdf in youe hand"} />
+    </div>
+    </>
+  );
+}
---
+++ app/upload-documents/page.tsx
@@ -0,0 +1,7 @@
+export default function DocumentUploadPage(){
+    return (
+        <>
+            <h1 className="text-center m-20">Upload your document here</h1>
+        </>
+    )
+}
