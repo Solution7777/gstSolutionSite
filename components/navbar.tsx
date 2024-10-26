eb1765085327489314c7433b669ee518e77cbd44
Solution7777 <soumen.gstsolution@gmail.com>
2024-10-26 19:02:35 +0530

    push from gstsolution center

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,51 +5,6 @@ import Link from "next/link";
 export default function Navbar(){
    
     return (
-<<<<<<< HEAD
-        <>       
-
-{/* <nav className="bg-white border-gray-200 dark:bg-gray-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gst Solution</span>
-    </Link>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <Link href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
-        </li>
-        <li>
-          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
-        </li>
-        <li>
-          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
-        </li>
-        <li>
-          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</Link>
-        </li>
-        <li>
-          <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav> */}
-<div className="flex justify-between m-4">
-<Link href={"/"}><h1 className="text-2xl font-extrabold bg-gradient-to-r from-orange-500 via-yellow-500 to-green-500 bg-clip-text text-transparent">GST Solution</h1></Link>
-<div>
-<Link href={"/register"} className="p-2" >Register</Link>
-<Link href={"/login"} className="p-2" >Login</Link>
-</div>
-</div>
-
-=======
         <> 
         <div className="flex m-2 p-2 justify-between">
          <Link href={"/"}><h1 className="px-4 text-2xl font-extrabold">GST Solution</h1></Link> 
@@ -58,7 +13,6 @@ export default function Navbar(){
          <Link href={"/login"} className="px-2">Login</Link>     
         </div>
         </div>   
->>>>>>> 74b6025558f2dd4ca3c475771c7e056001eb17a1
         </>
     )
 }
