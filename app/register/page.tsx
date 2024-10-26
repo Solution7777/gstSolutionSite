b81d14562428fd9e1c8fe1bb632f52221755c6b1
Solution7777 <soumen.gstsolution@gmail.com>
2024-10-26 20:05:08 +0530

    change resister

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,10 @@
-import { registerUser } from "@/lib/actions";
+import { signUp } from "@/lib/auth"
 
 export default function Register(){
     return (
         <>       
 
-<form className="max-w-md mx-auto" action={registerUser}>
+<form className="max-w-md mx-auto" action={signUp}>
   <div className="relative z-0 w-full mb-5 group">
       <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " required />
       <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
